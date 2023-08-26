9913bee76c667282241cc13215a862ec6701eead
Giorgi Tsintsadze <giotsintsadze94@gmail.com>
2023-08-26 15:45:41 +0400

    updated solution

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
-DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE grade >= 4);
-DELETE FROM payment WHERE student_id IN (SELECT id FROM student WHERE grade >= 4);
-DELETE FROM student WHERE grade >= 4;
+DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE groupnumber >= 4);
+DELETE FROM student WHERE groupnumber >= 4;
 DELETE FROM mark WHERE mark < 4 AND student_id IN (SELECT id FROM student);
-DELETE FROM payment WHERE type_id = (SELECT id FROM paymenttype WHERE name = 'DAILY');
-DELETE FROM paymenttype WHERE name = 'DAILY';
+DELETE FROM student WHERE id IN (SELECT student_id FROM mark);
+DELETE FROM payment WHERE type_id = (SELECT id FROM paymenttype WHERE name = 'Daily');
+DELETE FROM paymenttype WHERE name = 'Daily';
 DELETE FROM mark WHERE mark < 7;
